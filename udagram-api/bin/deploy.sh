92d692dfeb7b11d92c779507d5868d05fd7bc79d
yosif-khaled <yosif.khaledjm@gmail.com>
2022-03-22 08:11:22 +0200

    deploying api to elasticbean

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 # initializing eb
-eb init udagram-api --platform node.js --region us-east-1
+# eb init udagram-api --platform node.js --region us-east-1
 # setting eb environment
 eb use udagram-api-dev
 # deploying on eb
