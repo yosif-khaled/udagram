5f21ebbc04040b654fb849487d6797e429e5120b
yosif-khaled <yosif.khaledjm@gmial.com>
2022-03-22 19:10:28 +0200

    setting all variables in circleCI

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,15 @@
-# initializing eb
-# eb init udagram-api --platform node.js --region us-east-1
-# setting eb environment
+# using an environment
 eb use udagram-api-dev --profile default
+# setting up environment variables
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+AWS_BUCKET=$AWS_BUCKET \
+AWS_REGION=$AWS_DEFAULT_REGION \
+SECRET_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY \
+JWT_SECRET=$JWT_SECRET \
+POSTGRES_DB=$POSTGRES_DB \
+POSTGRES_HOST=$POSTGRES_HOST \
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+POSTGRES_PORT=$POSTGRES_PORT \
+POSTGRES_USERNAME=$POSTGRES_USERNAME
 # deploying on eb
 eb deploy udagram-api-dev --profile default
---
+++ udagram-api/bin/old.sh
@@ -0,0 +1,6 @@
+# initializing eb
+# eb init udagram-api --platform node.js --region us-east-1
+# setting eb environment
+eb use udagram-api-dev --profile default
+# deploying on eb
+eb deploy udagram-api-dev --profile default
