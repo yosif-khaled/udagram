ea42cf844bf6fdf9373d6bbad66f9d4748586cc5
yosif-khaled <yosif.khaledjm@gmail.com>
2022-03-22 11:57:24 +0200

    deploying api to elasticbean

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -13,7 +13,5 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
+  jwt_secret: process.env.JWT_SECRET,
 };
